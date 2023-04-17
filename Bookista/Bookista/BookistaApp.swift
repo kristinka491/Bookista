3f2d9313076b6bfc290d95f1c947dd305c6b8d19
Krystsina Suvorava <>
2023-04-17 12:33:59 +0300

    Project was created

---
+++ Bookista/Bookista/BookistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookistaApp.swift
+//  Bookista
+//
+//  Created by Krystsina on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct BookistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
